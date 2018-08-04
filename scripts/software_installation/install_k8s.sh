5d24b16ac9f286256dfb504cbe755fcf7e21179c
berton1982 <berton.jiang@outlook.com>
2018-08-05 00:03:53 +0800

    Update install_k8s.sh
    install docker community version

--- scripts/software_installation/install_k8s.sh
+++ scripts/software_installation/install_k8s.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 # install docker community version, kubernetes & etc.
+
+
+# install docker community version
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum -y install docker-ce
+systemctl enable docker  
+# add daocloud mirror
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://791bb63a.m.daocloud.io
+systemctl start docker
