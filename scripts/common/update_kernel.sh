07f6f7fd028268823ee05e1955ddfbdecd43d8e6
berton1982 <berton.jiang@outlook.com>
2018-08-04 22:38:02 +0800

    Update update_kernel.sh
    add todo

--- scripts/common/update_kernel.sh
+++ scripts/common/update_kernel.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# todo : resolve problem 
 echo `rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org &&
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm && 
-yum -y --enablerepo=elrepo-kernel -y install kernel-ml.x86_64` | sudo sh
+yum -y --enablerepo=elrepo-kernel -y install kernel-ml.x86_64 &&
+grub2-set-default 0` | sudo sh
