77b9590286597f1fa3da714ee3055376590f3b41
berton1982 <berton.jiang@outlook.com>
2018-08-06 22:20:54 +0800

    Update install_docker.sh
    update docker installation script

--- scripts/software_installation/install_docker.sh
+++ scripts/software_installation/install_docker.sh
@@ -3,10 +3,10 @@
 
 
 # install docker community version
-yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum -y install docker-ce
-systemctl enable docker  
-# add daocloud mirror
-curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://791bb63a.m.daocloud.io
-systemctl start docker
+echo `yum install -y yum-utils device-mapper-persistent-data lvm2 &&
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &&
+yum -y install docker-ce &&
+systemctl enable docker &&
+gpasswd -a $USER docker &&
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://791bb63a.m.daocloud.io &&
+systemctl start docker` | sudo sh && newgrp docker && docker ps
