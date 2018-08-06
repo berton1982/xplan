9ed737e1388bf4ac01e485fb968208e5be381ad6
berton1982 <berton.jiang@outlook.com>
2018-08-06 22:10:53 +0800

    Update install_k8s.sh

--- scripts/software_installation/install_k8s.sh
+++ scripts/software_installation/install_k8s.sh
@@ -3,10 +3,18 @@
 
 
 # install docker community version
-yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum -y install docker-ce
-systemctl enable docker  
+echo `yum install -y yum-utils device-mapper-persistent-data lvm2 &&
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &&
+yum -y install docker-ce &&
+systemctl enable docker` | sudo sh
+
+# add docker group and add current user to this group. for The docker daemon binds to a Unix socket instead of a TCP port. By default that Unix socket is owned by the user root and other users can only access it using sudo. The docker daemon always runs as the root user.
+# If you don’t want to use sudo when you use the docker command, create a Unix group called docker and add users to it. When the docker daemon starts, it makes the ownership of the Unix socket read/writable by the docker group.
+# sudo groupadd docker
+sudo gpasswd -a $USER docker
+newgrp docker
+
+
 # add daocloud mirror
-curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://791bb63a.m.daocloud.io
-systemctl start docker
+echo `curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://791bb63a.m.daocloud.io &&
+systemctl start docker` | sudo sh
