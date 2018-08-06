eb9e22daea25a0f2bd6619bdee99227805192ccb
berton1982 <berton.jiang@qq.com>
2018-08-06 12:01:09 -0400

    update install_k8s.sh

--- scripts/software_installation/install_k8s.sh
+++ scripts/software_installation/install_k8s.sh
@@ -8,18 +8,24 @@ baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64
 enabled=1
 gpgcheck=0
 EOF
+
+#shutdown selinux
+cat /etc/sysconfig/selinux | sed '/^SELINUX=/c\SELINUX=disabled' > a
+
 if [ $UID -ne 0 ]
 then
   echo 'switch to sudo'
-  echo 'mv a.repo /etc/yum.repos.d/kubernetes.repo &&
+  echo 'mv a.repo /etc/yum.repos.d/kubernetes.repo &&    
     setenforce 0 &&
     yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes &&
+    systemctl disable firewall &&
     systemctl enable kubelet && 
     systemctl start kubelet' | sudo sh
 else
-  mv a.repo /etc/yum.repos.d/kubernetes.repo &&
+  mv a.repo /etc/yum.repos.d/kubernetes.repo
   setenforce 0
   yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
   systemctl enable kubelet
   systemctl start kubelet
+
 fi
