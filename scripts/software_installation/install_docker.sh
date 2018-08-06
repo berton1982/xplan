7e844ea7d4fe70d1f096bb8d128f3f0db03ce2bd
berton1982 <berton.jiang@qq.com>
2018-08-06 10:35:56 -0400

    update install docker script

--- scripts/software_installation/install_docker.sh
+++ scripts/software_installation/install_docker.sh
@@ -3,10 +3,21 @@
 
 
 # install docker community version
-echo `yum install -y yum-utils device-mapper-persistent-data lvm2 &&
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &&
-yum -y install docker-ce &&
-systemctl enable docker &&
-gpasswd -a $USER docker &&
-curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://791bb63a.m.daocloud.io &&
-systemctl start docker` | sudo sh && newgrp docker && docker ps
+if [ $UID -ne 0]
+then
+	echo 'switch to sudo'
+	echo 'yum install -y yum-utils device-mapper-persistent-data lvm2 &&
+	  yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &&
+	  yum -y install docker-ce &&
+	  systemctl enable docker &&
+	  gpasswd -a $USER docker &&
+	  curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://791bb63a.m.daocloud.io &&
+	  systemctl start docker' | sudo sh && newgrp docker && docker ps
+else	
+	yum install -y yum-utils device-mapper-persistent-data lvm2
+	yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+	yum -y install docker-ce 
+	systemctl enable docker 
+	curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://791bb63a.m.daocloud.io 
+	systemctl start docker
+fi
