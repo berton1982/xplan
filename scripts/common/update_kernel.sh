12d94d61fc6be881ed35e849936a2a75d8fe8c85
berton1982 <berton.jiang@outlook.com>
2018-08-06 22:32:06 +0800

    Update update_kernel.sh
    update kernel from normal user.(has sudo permission)

--- scripts/common/update_kernel.sh
+++ scripts/common/update_kernel.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# todo : resolve problem 
-echo `rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org &&
-rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm && 
-yum -y --enablerepo=elrepo-kernel -y install kernel-ml.x86_64 &&
-grub2-set-default 0` | sudo sh
+# todo : resolve problem
+if [ $UID -ne 0]
+then
+  echo 'rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org &&
+      rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm && 
+      yum -y --enablerepo=elrepo-kernel -y install kernel-ml.x86_64 &&
+      grub2-set-default 0' | sudo sh
+else
+  rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+  rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+  yum -y --enablerepo=elrepo-kernel -y install kernel-ml.x86_64
+  grub2-set-default 0
+fi
