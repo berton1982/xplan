69d4122d09fd3b4cfb9b97bc1e6065e2cf7497e9
berton1982 <berton.jiang@qq.com>
2018-08-08 11:20:01 -0400

    update kube_version

--- scripts/software_installation/pull_k8s_images.sh
+++ scripts/software_installation/pull_k8s_images.sh
@@ -2,7 +2,7 @@
 
 # pre download docker images for k8s.  
 # solve unable to get URL "https://dl.k8s.io/release/stable-1.11.txt" ....
-K8S_VERSION=v1.11.0
+K8S_VERSION=v1.11.1
 ETCD_VERSION=3.1.12
 DASHBOARD_VERSION=v1.8.3
 FLANNEL_VERSION=v0.10.0-amd64
@@ -26,3 +26,6 @@ do
   docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
   docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
 done
+
+echo "all images downloaded"
+docker images
