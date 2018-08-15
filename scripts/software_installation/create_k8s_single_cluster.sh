209ddae09bf5f5c4759ff1fc2b412e291abd5172
berton1982 <berton.jiang@outlook.com>
2018-08-15 22:21:06 +0800

    Update create_k8s_single_cluster.sh
    sudo create_single_cluster

--- scripts/software_installation/create_k8s_single_cluster.sh
+++ scripts/software_installation/create_k8s_single_cluster.sh
@@ -5,8 +5,25 @@ k8s_version=$(kubeadm version | gawk -F, '{print $3}' | gawk -F: '{print $2'} | 
 # using calico, and using master ip
 if [ $UID -eq 0 ]
 then
+  kubeadm reset && etcdctl del "" --prefix &&
   kubeadm init --pod-network-cidr=192.168.0.0/16 --kubernetes-version=$k8s_version
+  export KUBECONFIG=/etc/kubernetes/admin.conf
+  # install a pod network add-on calico
+  kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+  kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+  # allow schedule pods on master.
+  kubectl taint nodes --all node-role.kubernetes.io/master-
 else
   echo 'switch to sudo'
-  echo "kubeadm init --pod-network-cidr=192.168.0.0/16 --kubernetes-version=${k8s_version} --ignore-preflight-errors='IsPrivilegedUser,Swap'" | sh
+  # reset environment and del etcd data. and init cluster as root
+  echo "kubeadm reset && etcdctl del "" --prefix && kubeadm init --pod-network-cidr=192.168.0.0/16 --kubernetes-version=${k8s_version} --ignore-preflight-errors='IsPrivilegedUser,Swap'" | sudo sh
+  To make kubectl work for your non-root user, run these commands, which are also part of the kubeadm init output
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  # install a pod network add-on calico
+  kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+  kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+  # allow schedule pods on master.
+  kubectl taint nodes --all node-role.kubernetes.io/master-
 fi
