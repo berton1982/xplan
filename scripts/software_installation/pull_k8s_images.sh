1b2d1bf2fe1d1810ef128b4db1a15960dc6a8b65
berton1982 <berton.jiang@outlook.com>
2018-08-15 21:50:56 +0800

    Update pull_k8s_images.sh
    add coredns, pause, update etcd-amd64

--- scripts/software_installation/pull_k8s_images.sh
+++ scripts/software_installation/pull_k8s_images.sh
@@ -3,11 +3,12 @@
 # pre download docker images for k8s.  
 # solve unable to get URL "https://dl.k8s.io/release/stable-1.11.txt" ....
 K8S_VERSION=v1.11.1
-ETCD_VERSION=3.1.12
+ETCD_VERSION=3.2.18
 DASHBOARD_VERSION=v1.8.3
 FLANNEL_VERSION=v0.10.0-amd64
 DNS_VERSION=1.14.8
 PAUSE_VERSION=3.1
+COREDNS_VERSION=1.1.3
 
 images=(kube-proxy-amd64:$K8S_VERSION 
   kube-scheduler-amd64:$K8S_VERSION 
@@ -15,6 +16,7 @@ images=(kube-proxy-amd64:$K8S_VERSION
   kube-apiserver-amd64:$K8S_VERSION
   etcd-amd64:$ETCD_VERSION 
   pause-amd64:$PAUSE_VERSION 
+  pause:$PAUSE_VERSION
   kubernetes-dashboard-amd64:$DASHBOARD_VERSION 
   k8s-dns-sidecar-amd64:$DNS_VERSION 
   k8s-dns-kube-dns-amd64:$DNS_VERSION
