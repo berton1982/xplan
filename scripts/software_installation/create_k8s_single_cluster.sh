5ddbe6f28ad9563dcbe0c2089917e3dcb6b0cf37
berton1982 <berton.jiang@qq.com>
2018-08-15 09:38:13 -0400

    rm some empty dirs

--- scripts/software_installation/create_k8s_single_cluster.sh
+++ scripts/software_installation/create_k8s_single_cluster.sh
@@ -1,14 +1,12 @@
 #/bin/bash
 
-#tell if command jq exists, if not exists, download
-type /user/bin/jq 2>/dev/null || echo 'curl -o jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 && mv jq /user/bin/jq && chmod a+x /user/bin/jq' | sudo sh
-
-k8s_version = $(kubeadm version | gawk -F, '{print $3}' | gawk -F: '{print $2'} | sed 's/"//g')
+k8s_version=$(kubeadm version | gawk -F, '{print $3}' | gawk -F: '{print $2'} | sed 's/"//g')
 
 # using calico, and using master ip
-if [ $UID ==0 ]
+if [ $UID -eq 0 ]
 then
   kubeadm init --pod-network-cidr=192.168.0.0/16 --kubernetes-version=$k8s_version
 else
-  echo 'kubeadm init --pod-network-cidr=192.168.0.0/16 --kubernetes-version=$k8s_version' | sudo sh
+  echo 'switch to sudo'
+  echo "kubeadm init --pod-network-cidr=192.168.0.0/16 --kubernetes-version=${k8s_version} --ignore-preflight-errors='IsPrivilegedUser,Swap'" | sh
 fi
--- scripts/users/user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# description
-# check mt_group_* groups and their group members
-# maintain these groups
-
-# check groups and list their members
-function check_groups {
-	local
-}
