47aca4db5d0680b3ada028afa04c3143adc73ab4
berton1982 <berton.jiang@outlook.com>
2018-08-17 00:30:37 +0800

    Update install_helm.sh

--- scripts/software_installation/install_helm.sh
+++ scripts/software_installation/install_helm.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+# install helm
+# if failed to download, check dns
+# replace kubernetes-helm.storage.googleapis.com as storage.googleapis.com/kubernetes-helm
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | sed 's/kubernetes-helm.storage.googleapis.com/storage.googleapis.com\/kubernetes-helm' | sh
