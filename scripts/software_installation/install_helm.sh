8e258aece5f908641547326ed16d9494aba61a83
berton1982 <berton.jiang@qq.com>
2018-08-16 12:37:43 -0400

    fix sed bug for install_helm.sh

--- scripts/software_installation/install_helm.sh
+++ scripts/software_installation/install_helm.sh
@@ -2,4 +2,4 @@
 # install helm
 # if failed to download, check dns
 # replace kubernetes-helm.storage.googleapis.com as storage.googleapis.com/kubernetes-helm
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | sed 's/kubernetes-helm.storage.googleapis.com/storage.googleapis.com\/kubernetes-helm' | sh
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | sed 's/kubernetes-helm.storage.googleapis.com/storage.googleapis.com\/kubernetes-helm/' | sh
